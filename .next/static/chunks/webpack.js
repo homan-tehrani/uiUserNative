100c43c893d29e2ce106c7d4af3598892cc24c12
motallebi.aliakbar <motallebi.aliakbar@gmail.com>
2023-04-15 15:47:49 +0330

    fix slider 3d

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "552e190c54a0d0c5"; }
+/******/ 		__webpack_require__.h = function() { return "08f2685ce396301e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
